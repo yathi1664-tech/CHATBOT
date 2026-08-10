444e8c2c8559f8de4a1345b96ff2a105d49da113
yathi1664-tech <yathi1664@gmail.com>
2026-08-11 01:25:35 +0530

    Delete private-chat-upgraded-v10-private-notifications directory

--- private-chat-upgraded-v10-private-notifications/scripts/generate-vapid.js
+++
@@ -1,6 +0,0 @@
-const webpush = require('web-push');
-const keys = webpush.generateVAPIDKeys();
-console.log('\nAdd these to Render -> Environment:\n');
-console.log('VAPID_PUBLIC_KEY=' + keys.publicKey);
-console.log('VAPID_PRIVATE_KEY=' + keys.privateKey);
-console.log('\nKeep VAPID_PRIVATE_KEY secret. Do not put it in GitHub.\n');
